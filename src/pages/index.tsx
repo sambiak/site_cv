1afad8ba71af58f3b26d197706ea54a7b85c0748
guillaume <guillaume.augustoni@epfl.ch>
2023-03-12 02:00:01 +0100

    minimalist

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,21 @@ export default function Home() {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚧</text></svg>"/>
       </Head>
-      <main >
+      <main className={styles.main}>
           <div className={styles.hook}>
           <h3>Un <div className={styles.nom}><h3>développeur Java</h3> </div> ou un <div className={styles.nom}><h3>ingénieur en science des données</h3> </div> ? </h3>
           <h3>Ne cherchez pas plus loin, c&apos;est <div className={styles.nom}><h3>Guillaume Augustoni</h3> </div> qu&apos;il vous faut !</h3>
+              <div >
+                  <p >Je suis un ingénieur en sciences des données diplomé de l&amp;EPFL</p>
+                  <p>Mais aussi un développeur java</p>
+              </div>
+
+              <div>
+                  <ul>
+                      <li><a href="/cv">Curriculum Vitae</a></li>
+                      <li><a href="/blog">Blog</a></li>
+                  </ul>
+              </div>
       </div>
       </main>
     </>
